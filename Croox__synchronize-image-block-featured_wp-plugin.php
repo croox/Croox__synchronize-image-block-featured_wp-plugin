5d776954529a5865b9e7cb4b41741ef1047f7f70
jhotadhari <tellme@waterproof-webdesign.info>
2020-09-12 12:26:24 +0530

    Rename

--- Croox__synchronize-image-block-featured_wp-plugin.php
+++ Croox__synchronize-image-block-featured_wp-plugin.php
@@ -1,8 +1,8 @@
 <?php
 /*
-	Plugin Name: Synchronize image block with featured image
+	Plugin Name: Croox Synchronize image block with featured image
 	Plugin URI: https://github.com/croox/Croox__synchronize-image-block-featured_wp-plugin
-	Description: Adds an option to synchronize a core/image block with the post featured image.
+	Description: Add option to synchronize the core/image block with the post featured image.
 	Version: 0.0.0
 	Author: croox
 	Author URI: https://github.com/croox
